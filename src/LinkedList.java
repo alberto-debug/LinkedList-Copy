f607dac23409c66f09769c20d675b2c2e70ef80f
Alberto <albertojunior848423803@gmail.com>
2024-11-13 22:46:28 +0300

    change

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -2,22 +2,15 @@ public class LinkedList {
 
     Node head;
 
-    public void insert(int x) {
+   public void insert(int x){
 
-        Node newnode = new Node();
-        newnode.data = x;
-        newnode.next = null;
+       Node newnode = new Node();
+       newnode.data = x;
+       newnode.next = null;
 
-        if (head == null) {
-            head = newnode;
-        } else {
-            Node n = head;
-            while (n.next != null) {
-                n = n.next;
-            }
+       if (head == null){
+           head = newnode;
+       }
 
-            n.next = newnode;
-        }
-
-    }
+   }
 }
