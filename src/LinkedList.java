ad820f3bf00134de02e14a9d2bdbb8b61b721ca9
Alberto <albertojunior848423803@gmail.com>
2024-11-20 17:46:14 +0300

    change

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,6 +1,16 @@
 public class LinkedList {
 
-   
+    Node head;
+
+    public void insert(int x){
+
+        Node newnode = new Node();
+        newnode.data  = x;
+        newnode.next = null;
+
+
+    }
+
 }
 
 
