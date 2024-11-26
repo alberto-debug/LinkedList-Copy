6497a0140dd0e7e6f779c888eac99df83cde596d
Alberto <albertojunior848423803@gmail.com>
2024-11-26 04:48:50 +0300

    changes

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -2,32 +2,30 @@ public class LinkedList {
 
     Node head;
 
-    public void insert(int x){
+    public void insert(int x) {
 
         Node newnode = new Node();
         newnode.data = x;
         newnode.next = null;
 
-        if (head == null){
+        if (head == null) {
             head = newnode;
 
         } else {
             Node n = newnode;
-            while (n.next != null){
+            while (n.next != null) {
                 n = n.next;
             }
             n.next = newnode;
         }
     }
 
-    public void traverse(){
+    public void traverse() {
         Node v = head;
-        while (v.next != null){
+        while (v.next != null) {
             System.out.println(v.data);
-            v=v.next;
+            v = v.next;
         }
         System.out.println("Tail Node : " + v.data);
     }
 }
-
-
